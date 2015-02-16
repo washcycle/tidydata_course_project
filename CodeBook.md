3b17a0d5c5f54c0e86934c5abd00a5d06b78b257
Matt Landowski <litho23@gmail.com>
2015-02-15 22:25:37 -0600

    initial commit

---
+++ CodeBook.md
@@ -0,0 +1,48 @@
+# Getting and Cleaning Data Course Project Feb 2015
+This code book describes the variables, packages, and transformations performed in
+the run_analysis.R script.
+
+## Assumptions
+1. The data in the intertia folders (raw data) is not used as the y_train.txt and x_test.txt files contain calulated data. This project only calulated measurements which are contained in the y_train.txt and x_test.txt files.
+
+## Variables
+* **activities** - activity ID to activity text label mapping, loaded from activity_labels.txt
+* **features**  - descriptive column names for test and training data, loaded from features.txt
+* **test_set** - test set data from X_test.txt, columns names set using features data
+* **training_set** - training set data from X_train.txt, columns names set using features data
+* **training_subject_labels** - subject ID to training_set observation mapping
+* **test_subject_labels** - subject ID to test_set observation mapping
+* **test_set_activities** - activity to test_set data set
+* **training_set_activities** - activity to training_set data set
+* **test_set** - test set data with subject and activity variables
+* **training_set** - training set data with subject and activity variables
+* **fdata_set** - filtered data set containing only mean and standard deviation measurements
+* **tidydata** - final tidy data set with filtered data containing only the mean of the mean and standard deviation data for each subject and their respective activities. For this set a total of observations 180 observations from thirty subjects and six activites for each subject (3 * 6 = 180).
+
+
+## Transformations
+
+### Subjects
+The subject data for each data set is added as a new variable column, 'subject'. 
+
+### Descriptive labels
+The test_set_activities and training_set_activities data frames are looped through by the activityID in the activites data frame. If an activityID is found the numeric value is swapped with the correct descriptive label from the activites data frame.
+
+Then the activity columns are converted into factors.
+
+Last the test_set_activites and training_set_activities with descriptive labeling is added as a new variable column to their respective data sets.
+
+### Merged data sets
+merge function is used to merge the test_set and training_set into data_set data frame.
+
+## Filtering
+data_set is filtered by looking for columns names with only, 'mean' or 'std' in them, ignoring case. The filtered data is saved into the fdata_set data frame.
+
+The filtered data set is then arranged by subject.
+
+
+## Tidy Data Output
+Aggregate function calulates the mean of each measurement with respect to the subject and activity.
+To differentiate the columns '.Mean' is appended to the end of each measurement variable.
+
+write.table is called to save the tidydata save the data to a file called tidydata.txt
---
+++ run_analysis.R
@@ -0,0 +1,71 @@
+library(plyr)
+
+## Getting and Cleaning Data John Hopkins Coursera Course Project
+# Data is from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## 1 Merges the training and the test sets to create one data set.
+
+# get the activity labels and store them in memory
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", 
+                              col.names =  c("activityID", "activity"),
+                              stringsAsFactors = F)
+
+features <- read.table("UCI HAR Dataset/features.txt", 
+                              col.names =  c("featureID", "features"),
+                              stringsAsFactors = F)
+
+# Read the measurement data from files
+test_set <-     read.table("UCI HAR Dataset//test/X_test.txt", col.names = features$features)
+training_set <- read.table("UCI HAR Dataset//train/X_train.txt", col.names = features$features)
+
+# Load subjects labels for training and test sets
+training_subject_labels <- read.table("UCI HAR Dataset//train/subject_train.txt", 
+                                col.names = c("subject"))
+test_subject_labels <-     read.table("UCI HAR Dataset//test/subject_test.txt", 
+                                col.names = c("subject"))
+
+# Add subject variable to training and test data sets
+test_set$subject <- test_subject_labels$subject
+training_set$subject <- training_subject_labels$subject
+
+## Add activities to data sets
+test_set_activities <-     read.table("UCI HAR Dataset//test/y_test.txt", 
+                                      col.names = c("activity"))
+training_set_activities <- read.table("UCI HAR Dataset//train/y_train.txt", 
+                                      col.names = c("activity"))
+
+# Convert activity integer value to descriptive label
+for( id in activities$activityID ){
+    test_set_activities$activity[test_set_activities$activity == id] <- 
+        activities$activity[activities$activityID == id]
+    training_set_activities$activity[training_set_activities$activity == id] <- 
+        activities$activity[activities$activityID == id]
+}
+
+# Add activity variable to training and test set
+test_set$activity <- factor(test_set_activities$activity)
+training_set$activity <- factor(training_set_activities$activity)
+
+# merge the dataset
+data_set <- merge(test_set, training_set, all = T)
+
+
+## 2 Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+# Filter sets by only mean and standard deviation measurements
+fdata_set <- (data_set[grep("mean|std|activity|subject",
+                              names(training_set), 
+                              ignore.case = T)])
+
+fdata_set <- arrange(fdata_set, subject)
+
+## 5 From the data set in step 4, creates a second, independent tidy data set with 
+# the average of each variable for each activity and each subject.
+tidydata <- aggregate(. ~ subject + activity, data = fdata_set, mean)
+# appending Mean to the end of each column name
+names(tidydata)[c(-1,-2)] <- sapply(names(tidydata)[c(-1,-2)], paste, 'Mean', sep=".")
+
+# arrange by subject then by activity with respect to each subject
+tidydata <- arrange(tidydata, subject, activity))
+
+write.table(tidydata, "tidydata.txt", row.name=FALSE)
